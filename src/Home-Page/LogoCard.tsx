13d18bca9a073f85d0584e293290c2f1ca581f04
Caleb <caleblee12694@gmail.com>
2023-02-12 00:48:45 -0500

    Added Logo on Landing Page

--- src/Home-Page/LogoCard.tsx
+++ src/Home-Page/LogoCard.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "react-bootstrap";
 import Card from "react-bootstrap/Card";
+import logo from "./logo.png";
 
 export default function LogoCard() {
   return (
-    <div>
-     <Card className="align-items-center border-0 bg-transparent"
-     style={{ width: '5rem', height: '7rem' }}>
-        <Card.Img variant="top" src="https://www.i2symbol.com/images/symbols/math/infinity_u221E_icon_256x256.png"/>
-        <h4 className="card-text mb-5" >TAP</h4>
-    </Card>
+     <div className="align-items-start d-flex">
+        <img src={logo} width="75"/>
+        <h4 className="card-text mb-5 font-bold text-6xl" style={{fontFamily: '"Futura", san-serif', padding: "2.5px"}}>
+          TAP
+        </h4>
     </div>
   );
 }
--- src/Home-Page/WelcomePart2.tsx
+++ src/Home-Page/WelcomePart2.tsx
@@ -10,6 +10,9 @@ export default function WelcomePage() {
   return (
     <div>
     <section className="bg-gray-900 text-white">
+    <div style={{position: "absolute", top: "20px", left: "20px", padding: "0px" }}>
+        <LogoCard />
+      </div>
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
         <div className="mx-auto max-w-3xl text-center">
           <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
@@ -19,14 +22,14 @@ export default function WelcomePage() {
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
-              className="font-bold block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto no-underline"
+              className="font-bold block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto no-underline"
               href="login"
             >
               Login
             </a>
 
             <a
-              className="font-bold block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto no-underline"
+              className="font-bold block w-full rounded border border-blue-600 px-12 py-3 text-sm text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto no-underline"
               href="/about"
             >
               Learn More
