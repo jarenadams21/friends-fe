8f0b7d66f48b8e3c6ce4560b2a27429e0d5c2aca
Win Tongtawee <WinTongtawee@gmail.com>
2023-02-12 06:35:40 -0500

    BUG FIX

--- src/pages/Profile/ProfileScreen.tsx
+++ src/pages/Profile/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './Profile.css';
+import './profile.css';
 import { Button, Form, Modal, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
 import { url } from '../../constants';
