8e6a206cec7420d68bf931677213b7183c13c3da
Win Tongtawee <WinTongtawee@gmail.com>
2023-02-12 05:31:42 -0500

    DAVID IS MY DADDY

--- src/pages/Login-Register/Login.tsx
+++ src/pages/Login-Register/Login.tsx
@@ -1,21 +1,21 @@
-import { useState } from 'react';
-import { Button, Form, InputGroup } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
-import './Registration.css'
-import axios from 'axios';
-import { url } from '../../constants';
+import { useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import "./Registration.css";
+import axios from "axios";
+import { url } from "../../constants";
 import backdrop from "./backdrop.jpg";
 import LogoCard from "../../Home-Page/LogoCard";
 
 interface User {
-  username: string,
-  password: string,
+  username: string;
+  password: string;
 }
 
 function LoginScreen() {
   const nav = useNavigate();
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const [passwordShown, setPasswordShown] = useState(false);
 
   const togglePassword = () => {
@@ -23,67 +23,85 @@ function LoginScreen() {
   };
 
   const handleLogin = (username: string, password: string) => {
-    axios.get(url + "users/login", {
-      params: {
-        username: username,
-        password: password,
-      }
-    }
-    )
-    .then(res => {
-      console.log(res);
-      const id = res.data.toString();
-      nav("/feed/" + id)
-    }) 
-    .catch(err => console.log(err));
-  }
+    axios
+      .get(url + "users/login", {
+        params: {
+          username: username,
+          password: password,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        const id = res.data.toString();
+        nav("/feed/" + id);
+      })
+      .catch((err) => console.log(err));
+  };
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     handleLogin(username, password);
-  }
+  };
 
   return (
-    <div style={{ display: "flex", justifyContent: "space-between"}}>
+    <div style={{ display: "flex", justifyContent: "space-between", height: "92vh"}}>
       <div className="Container">
         <div className="pt-28 px-8">
-        <header className="Header font-sans font-bold">Log in</header>
-        <hr className="pt-0" />
-        <Form onSubmit={onSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control
-              type="username"
-              placeholder="Username"
-              onChange={e => {
-                const val = e.target.value;
-                setUsername(val);
-              }} />
-          </Form.Group>
-          <InputGroup className="mb-3">
-            <Form.Control
-              type={passwordShown ? "text" : "password"}
-              placeholder="Password"
-              onChange={e => {
-                const val = e.target.value;
-                setPassword(val);
-              }} />
-            <Button variant="outline-secondary" id="addon" onClick={togglePassword} className="ShowButton">
-              <i className={passwordShown ? "fas fa-eye-slash" : "fas fa-eye"} />
+          <header className="Header font-sans font-bold">Log in</header>
+          <hr className="pt-0" />
+          <Form onSubmit={onSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="username"
+                placeholder="Username"
+                onChange={(e) => {
+                  const val = e.target.value;
+                  setUsername(val);
+                }}
+              />
+            </Form.Group>
+            <InputGroup className="mb-3">
+              <Form.Control
+                type={passwordShown ? "text" : "password"}
+                placeholder="Password"
+                onChange={(e) => {
+                  const val = e.target.value;
+                  setPassword(val);
+                }}
+              />
+              <Button
+                variant="outline-secondary"
+                id="addon"
+                onClick={togglePassword}
+                className="ShowButton"
+              >
+                <i
+                  className={passwordShown ? "fas fa-eye-slash" : "fas fa-eye"}
+                />
+              </Button>
+            </InputGroup>
+            <Button
+              className="font-sans font-bold"
+              variant="danger"
+              type="submit"
+            >
+              Login
             </Button>
-          </InputGroup>
-          <Button className="font-sans font-bold" variant="danger" type="submit">
-            Login
-          </Button>
-        </Form>
-        <div className="SwitchLoginCreateGroup font-sans">
-          <p>Don't have an account?</p>
-          <Link to="/registration/" className="font-sans font-bold">Register</Link>
+          </Form>
+          <div className="SwitchLoginCreateGroup font-sans">
+            <p>Don't have an account?</p>
+            <Link to="/registration/" className="font-sans font-bold">
+              Register
+            </Link>
+          </div>
         </div>
+        <div className="relative bottom-0 left-5 text-slate-900">
+          <LogoCard />
         </div>
-        </div>
-        <div className="absolute bottom-0 left-36 text-slate-900"><LogoCard/></div>
-      <div>
-        <img src={backdrop}width="1500px"/>
+      </div>
+
+      <div className="bg-danger">
+        <img src={backdrop} width="1500px" />
       </div>
     </div>
   );
