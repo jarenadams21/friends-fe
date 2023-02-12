515e6313282bd05b159c781ba2b227e6879b6db8
Win Tongtawee <WinTongtawee@gmail.com>
2023-02-12 08:44:45 -0500

    banner

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -4,17 +4,6 @@ import EventCardsList from './Feed/EventCardsList';
 import Navbar from './Navbar';
 import {useParams} from 'react-router-dom';
 
-function FeedBanner() {
-return (
-    <Container>
-        <img 
-    className="mb-5"
-    src="https://a.espncdn.com/photo/2010/0929/nba_g_hornets01_576.jpg"
-    />
-    </Container>
-)
-}
-
 
 
 function Feed() {
@@ -25,7 +14,6 @@ function Feed() {
     return (<div className="bg-indigo-300">
     
     <Navbar id={id}/>
-    <FeedBanner/>
     <EventCardsList/>
 
     </div>
