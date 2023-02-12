1cccc59221bc2f68f1e58f695810668ccaba809f
Caleb <caleblee12694@gmail.com>
2023-02-11 22:42:17 -0500

    TeamMembers2.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
-import Feed from "./pages/Feed";
-import Welcome2 from "./Home-Page/WelcomePart2";
-import Welcome3 from "./Home-Page/WelcomePart3";
-
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from './pages/Navbar';
+//import Home from './pages/Home';
+import MyPlans from './pages/MyPlans'
 //import LoginScreen from './Login/login-screen';
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import RegistrationScreen from "./pages/Login-Register/registration-screen";
 import LoginScreen from "./pages/Login-Register/login-screen";
 import Profile from "./pages/Profile/profile";
+import Feed from  "./pages/Feed";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          {/* <Route path="/" element={ <Home/> } /> */}
-
-          {/* Welcome1, Welcome2, Welcome3 are all different themes */}
-          <Route path="/" element={<Welcome2 />} />
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/registration" element={<RegistrationScreen />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/feed" element={<Feed />} />
-        </Routes>
-      </BrowserRouter>
+    <Navbar/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Feed/> } />
+        <Route path="/my-plans" element={ <MyPlans/> } />
+        <Route path="/login" element={ <LoginScreen/> } />
+        <Route path="/registration" element={ <RegistrationScreen/> } />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
